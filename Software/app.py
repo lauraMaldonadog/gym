4850b92d97c3f85d8813e54c2b0fe5143e9b7f00
lauraMaldonadog <lausofimaldonadog@gmail,com>
2024-05-27 15:52:28 -0500

    4do commit

--- Software/app.py
+++ Software/app.py
@@ -248,6 +248,6 @@ def view_routine(email):
     return render_template('view_routine.html', user=user, user_routine=user_routine)
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
