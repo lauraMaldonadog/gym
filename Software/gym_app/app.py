882fc13d50aba0a6247d03f065067d8bc7d29607
lauraMaldonadog <lausofimaldonadog@gmail,com>
2024-05-27 14:52:15 -0500

    2do commit

--- Software/gym_app/app.py
+++ Software/gym_app/app.py
@@ -190,4 +190,5 @@ def view_plans():
     return render_template('planes.html', planes=planes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
